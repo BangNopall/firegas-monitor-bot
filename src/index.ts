873d90073ecf13eeae6d94656ec810b95266d95e
BangNopall <craft6254@gmail.com>
2025-11-26 20:37:52 +0700

    Add project documentation for FireGas Monitor Bot, detailing project overview, folder structure, environment configuration, dependencies, command functionalities, and usage instructions.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, Context } from 'telegraf';
+import type { Update } from 'telegraf/types';
+import createDebug from 'debug';
 
 import { start, status, realtimeOff, realtimeOn } from './commands';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { development, production } from './core';
+
 import { initMqtt } from './services/mqtt';
 
+const debug = createDebug('bot:dev');
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
-const ENVIRONMENT = process.env.NODE_ENV || '';
-
 const bot = new Telegraf(BOT_TOKEN);
 
 initMqtt();
@@ -17,9 +17,17 @@ bot.command('status', status());
 bot.command('realtime_on', realtimeOn());
 bot.command('realtime_off', realtimeOff());
 
-//production mode
-export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
-  await production(req, res, bot);
+const startbot = async (bot: Telegraf<Context<Update>>) => {
+  const botInfo = (await bot.telegram.getMe()).username;
+
+  debug('Bot runs');
+  debug(`${botInfo} starting bot...`);
+
+  await bot.launch();
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
 };
-//development mode
-ENVIRONMENT !== 'production' && development(bot);
+
+
+startbot(bot);
